adafea20ff257a2213dc2bc9b2f11d85b037584b
Eugene_Grom <sixfingers@ukr.net>
2023-10-31 20:49:13 +0200

    object in progress...

--- HOME_6_23.10.23/main_object.js
+++ HOME_6_23.10.23/main_object.js
@@ -137,14 +137,14 @@ console.log(idInput2);
 {
     let arr = [1, 2, 3, 4, 5, "a", "b", "c"];
     //напишіть код, який використовуючи деструктуризацію покладе:
-    //парні числа в змінні even1, even2,
-    //непарні в odd1, odd2, odd3,
-    //Букви в окремий масив
+
     const arrNumbersPair = arr.filter(item => typeof item === 'number' && item % 2 === 0);
     const arrNumbersNotPair = arr.filter(item => typeof item === 'number' && item % 2 === 1);
+    //Букви в окремий масив
     const arrLetters = arr.filter(item => typeof item === 'string');
-
+    //парні числа в змінні even1, even2,
     const [even1, even2] = arrNumbersPair;
+    //непарні в odd1, odd2, odd3,
     const [odd1, odd2, odd3] = arrNumbersNotPair;
 
     console.log("Even numbers:", even1, even2);
@@ -165,16 +165,111 @@ console.log(idInput2);
         children: [{ name: 'Maria' }, { name: 'Nikolay' }]
     }
     //вийміть використовуючи деструктуризацію імена дітей у змінні name1 та name2
-    const {children: [{name: name1}, {name: name2}]} = obj;
+    const { children: [{ name: name1 }, { name: name2 }] } = obj;
     console.log(name1, name2);
 }
 
 //Destruct 3
 {
-    let arr = [1,2,3,4, 5,6,7,10];
-    //вийміть використовуючи деструктуризацію об'єктів два перші елементи 
-    //та довжину масиву в змінні a, b та length
+    let arr = [1, 2, 3, 4, 5, 6, 7, 10];
+    //вийміть використовуючи деструктуризацію об'єктів два перші елементи та довжину масиву в змінні a, b та length
     const [[a, b], length] = [arr, arr.length];
     console.log(a, b, length);
 }
+
+//Copy delete
+{
+    const cat = {
+        name: 'Mars',
+        age: 3,
+        sex: 'male',
+        hair: {
+            color: 'grey',
+            length: 'short',
+        }
+    }
+
+    let a = prompt('Введіть ключ, який ви не хочете копіювати:');
+    let { [a]: key, ...otherInfo } = cat;
+
+    console.log(otherInfo);
+}
+
+//Currency real rate
+{
+    fetch('https://open.er-api.com/v6/latest/USD')
+        .then(res => res.json())
+        .then(data => {
+            let [a, b, c] = [
+                prompt('Користувач вводить вхідну валюту:').toUpperCase(),
+                prompt('Користувач вводить валюту, в яку відбувається конвертація').toUpperCase(),
+                +prompt('Користувач вводить суму у вхідній валюті')];
+
+            if (a in data.rates) { a1 = data.rates[a] } else { return console.log('Невірно введена поточна валюта') };
+            if (b in data.rates) { b1 = data.rates[b] } else { return console.log('Невірно введена бажана валюта') };
+            c = +c;
+            if (isNaN(c)) { return console.log('Невірно введена кількість поточної валюти') }
+
+            const cross = (1 / a1) / (1 / b1);
+
+            // if (typeof c === 'number' && !isNaN(c)) {
+            let result = (c * cross);
+            console.log(`Вітаємо! Ви придбали ${result.toFixed(2)} ${b} за ${c.toFixed(2)} ${a}!`)
+            // } else { console.log('Помилка 3') };
+            console.log(data); // Вивчіть структуру, що отримується з сервера в консолі
+
+            //Currency drop down
+            let currencies = Object.keys(data.rates);
+            let str = "<select>"
+            str += currencies.reduce((a, b) => `-- ${a} --<option>-- ${b} --</option>`, '');
+            str += "</select>"
+            document.write(str) //document.write відобразить ваш HTML на сторінці
+        });
+}
+
+//Table
+{
+    const persons = [
+        {
+            name: 'Марія',
+            fatherName: 'Іванівна',
+            surname: 'Іванова',
+            sex: 'female'
+        },
+        {
+            name: 'Миколай',
+            fatherName: 'Іванович',
+            surname: 'Іванов',
+            age: 15
+        },
+        {
+            name: 'Петро',
+            fatherName: 'Іванович',
+            surname: 'Іванов',
+            married: true
+        },
+    ]
+    //Перший прохід - пошук колонок
+    const columnArr = [];
+    for (const pers of persons) {
+        for (const key in pers) {
+            if (!(columnArr.includes(key))) {
+                columnArr.push(key)
+            };
+        }
+    }
+    //Заголовок
+    let str = `<table style='border-collapse: collapse;'>`;
+    str += `<tr>`;
+    for (let i = 0; i < columnArr.length; i++) {
+        str += `<th style='border: 1px solid black; padding: 10px; background-color: lightgrey; font-weight: bold;'>${columnArr[i]}</th>`;
+    }
+    str += `</tr></table>`;
+    document.write(str);
+}
+
+
+
+
+
 
